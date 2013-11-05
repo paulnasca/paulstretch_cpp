7f5c3993abe420661ea0b808304b0e2b4b0048c5
Nasca Octavian PAUL <zynaddsubfx@yahoo.com>
2013-11-05 10:35:10 +0200

    Bugfixes:  - small error on GUI (reported by David Binderman)  - vorbis compilation fixes on Linux

--- compile_linux_fftw.sh
+++ compile_linux_fftw.sh
@@ -6,7 +6,7 @@ fluid -c GUI.fl
 fluid -c FreeEditUI.fl
 
 g++ -ggdb GUI.cxx FreeEditUI.cxx *.cpp Input/*.cpp Output/*.cpp `fltk-config --cflags` \
- `fltk-config --ldflags`  -laudiofile -lfftw3f -lvorbisenc -lvorbisfile -lportaudio -lpthread -lmad -lmxml -o $outfile
+ `fltk-config --ldflags`  -laudiofile -lfftw3f -lz -logg -lvorbis -lvorbisenc -lvorbisfile -lportaudio -lpthread -lmad -lmxml -o $outfile
 
 rm -f GUI.h GUI.cxx FreeEditUI.h FreeEditUI.cxx
 
--- compile_linux_fftw_jack.sh
+++ compile_linux_fftw_jack.sh
@@ -7,7 +7,7 @@ fluid -c FreeEditUI.fl
 
 g++ -ggdb GUI.cxx FreeEditUI.cxx *.cpp Input/*.cpp Output/*.cpp `fltk-config --cflags` \
  `fltk-config --ldflags` \
- -laudiofile -lfftw3f -lvorbisenc -lvorbisfile -lportaudio -lpthread -lmad -lmxml \
+ -laudiofile -lfftw3f -lz -logg -lvorbis  -lvorbisenc -lvorbisfile -lportaudio -lpthread -lmad -lmxml \
  `pkg-config --cflags --libs jack samplerate` \
  -DHAVE_JACK -DENABLE_RESAMPLING \
  -o $outfile
